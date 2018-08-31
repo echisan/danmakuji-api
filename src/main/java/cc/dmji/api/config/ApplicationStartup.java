8ea7b2e6f83144b29ba8899987bf568824605e4f
echisan <xuan1916152345@gmail.com>
2018-09-01 01:17:05 +0800

    添加启动应用时清除用户登录凭证的配置选项及其功能

--- src/main/java/cc/dmji/api/config/ApplicationStartup.java
+++ src/main/java/cc/dmji/api/config/ApplicationStartup.java
@@ -3,6 +3,7 @@ package cc.dmji.api.config;
 import cc.dmji.api.constants.RedisKey;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -11,11 +12,20 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 public class ApplicationStartup implements ApplicationListener<ApplicationReadyEvent> {
     private static final Logger logger = LoggerFactory.getLogger(ApplicationStartup.class);
 
+    @Value("${dmji.startup.clean-login-token-cache}")
+    private boolean cleanLoginTokenCache = false;
+
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         ConfigurableApplicationContext context = event.getApplicationContext();
         StringRedisTemplate stringRedisTemplate = context.getBean(StringRedisTemplate.class);
         stringRedisTemplate.delete(RedisKey.WATCH_EPISODE_ONLINE_EACH);
-        logger.info("clean watch_episode_online_each cache!!");
+        logger.info("clean watch episode online each cache finished");
+
+        if (cleanLoginTokenCache){
+            stringRedisTemplate.delete(RedisKey.LOGIN_TOKEN_KEY);
+            logger.info("clean login user token finished");
+        }
+
     }
 }
